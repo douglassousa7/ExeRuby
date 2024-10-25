238f0b19d5297e3104dfb521e49e99eb893245b2
Douglas <douglas7.dsc@gmail.com>
2024-10-25 13:53:16 -0300

    atualização

--- MeusExe/times.rb
+++ MeusExe/times.rb
@@ -1,15 +1,20 @@
 #exemplo 1 puts
-8.times {puts "Segue o lider "}
+# 8.times {puts "Segue o lider "}
 
-#exemplo 1 prits
-8.times {print "Segue o lider "}
+# #exemplo 1 prits
+# 8.times {print "Segue o lider "}
 
-#exemplo 2 puts
-5.times do
-  puts "seugue o lider"
-end
+# #exemplo 2 puts
+# 5.times do
+#   puts "seugue o lider"
+# end
 
 #exemplo 2 print
-5.times do
-  print "seugue o lider "
-end
+# 5.times do
+#   print "seugue o lider 
+# end 
+
+#exemplo 3 contador puts
+10.times do |contador|
+   puts "#{contador}"
+end 
