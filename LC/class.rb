02ded250b843aee14f7c77dbfbcfe183f6d66667
Douglas <douglas7.dsc@gmail.com>
2024-11-17 17:13:51 -0300

    versaõ 2.0

--- LC/class.rb
+++ LC/class.rb
@@ -1,17 +1,29 @@
-class Cachorro
-  def latir
-    puts "au au"
+# class Cachorro
+#   def latir
+#     puts "au au"
+#   end
+# end
+
+# class Gato
+#   def miar
+#     puts "miau"
+#   end
+# end
+
+# cachorro = Cachorro.new
+# cachorro.latir
+
+# Gato.new.miar 
+
+class Conta
+end
+
+class Banco
+  def criar_nova_conta
+    conta.new
   end
 end
 
-class Gato
-  def miar
-    puts "miau"
-  end
-end
+banco = Banco.new
 
-cachorro = Cachorro.new
-cachorro.latir
-
-Gato.new.miar 
-
+conta = criar_nova_conta
