e942f3d6c129782e80eff7ee9070d40e1fcb8991
Douglas <douglas7.dsc@gmail.com>
2024-11-19 09:44:33 -0300

    versão 2

--- LC/metodo_construtor.rb
+++ LC/metodo_construtor.rb
@@ -1,14 +1,33 @@
-class Pessoa
-  def initialize (nome, idade)
-    @nome = nome
-    @idade = idade 
-  end 
+# class Pessoa
+#   def initialize (nome, idade)
+#     @nome = nome
+#     @idade = idade 
+#   end 
 
-  def exibir_informacoes
-    puts "#{@nome} tem #{@idade} anos."
+#   def exibir_informacoes
+#     puts "#{@nome} tem #{@idade} anos."
+#   end
+# end
+
+# pessoa = Pessoa.new('Joana', 45)
+# pessoa.exibir_informacoes
+
+
+
+# exe 2
+
+
+class Pessoa
+  def initialize(nome, idade)
+    @nome = nome  # Variável de instância
+    @idade = idade
+  end
+
+  def apresentar
+    puts "Olá, meu nome é #{@nome} e tenho #{@idade} anos."
   end
 end
 
-pessoa = Pessoa.new('Joana', 45)
-pessoa.exibir_informacoes
-
+pessoa1 = Pessoa.new("Douglas", 25)
+pessoa1.apresentar
+# Saída: Olá, meu nome é Douglas e tenho 25 anos.
