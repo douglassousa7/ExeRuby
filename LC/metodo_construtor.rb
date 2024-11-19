31055b457daf7071c1e052af8446d9bb1d68dbc8
Douglas <douglas7.dsc@gmail.com>
2024-11-19 09:46:23 -0300

    versão 3

--- LC/metodo_construtor.rb
+++ LC/metodo_construtor.rb
@@ -17,17 +17,42 @@
 # exe 2
 
 
-class Pessoa
-  def initialize(nome, idade)
-    @nome = nome  # Variável de instância
-    @idade = idade
+# class Pessoa
+#   def initialize(nome, idade)
+#     @nome = nome  # Variável de instância
+#     @idade = idade
+#   end
+
+#   def apresentar
+#     puts "Olá, meu nome é #{@nome} e tenho #{@idade} anos."
+#   end
+# end
+
+# pessoa1 = Pessoa.new("Douglas", 25)
+# pessoa1.apresentar
+# # Saída: Olá, meu nome é Douglas e tenho 25 anos.
+
+
+
+
+# ex 3
+
+
+class Contador
+  def initialize
+    @contagem = 0
   end
 
-  def apresentar
-    puts "Olá, meu nome é #{@nome} e tenho #{@idade} anos."
+  def incrementar
+    @contagem += 1
+  end
+
+  def mostrar
+    puts "Contagem atual: #{@contagem}"
   end
 end
 
-pessoa1 = Pessoa.new("Douglas", 25)
-pessoa1.apresentar
-# Saída: Olá, meu nome é Douglas e tenho 25 anos.
+contador = Contador.new
+contador.incrementar
+contador.mostrar # Saída: Contagem atual: 1
+
